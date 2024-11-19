a8f02d2684fefb3e0fcfed96117b1ec255ff623b
Impu003 <impushreeyn@gmail.com>
2024-11-19 16:36:49 +0530

    Update sort.c
    removed fun

--- sort.c
+++ sort.c
@@ -96,10 +96,6 @@ void swap(int* a, int* b) {
     int temp = *a;
     *a = *b;
     *b = temp;
-}
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+
+
 }
